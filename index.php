1fc39cb05ba0a87995a6934bf7d86738a9176101
Luis Valencia <lvalencia@csumb.edu>
2018-02-01 01:27:35 +0000

    Added all files

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset=”utf-8” />
+        <title>Luis Valencia Personal Site</title>
+         <link href="CSS/styles.css" rel="stylesheet" type="text/css"/>
+         <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet"> 
+        
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header>
+            <h1>Luis Valencia</h1>
+        </header>
+        
+        <nav>
+            <hr width= 50%/>
+            <a href="index.html"><strong> <i>Home</i> </strong></a>
+            <a href = "about.html">About</a>
+            <a href = "contact.html"> Contact</a>
+        </nav>
+        <br/><br/>
+        
+        <main>
+            
+            <figure id = "me">
+                 <img src="img/juan_doe.png" alt = "Picture of Me">
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br/>
+                <p>Thank you for visiting my professional portfolio website.</p>
+                <p>I an a Software Engineer and have benn recently hired at the Acme Corporation</p>
+                <p>Feel free to contact me!</p>
+                <br/><br/>
+                
+                <em>"With ordinary talent and extraordinary <strong>perserverance</strong>, all things are attainable."</em><br/>
+                - Thomas F. Buxton
+            </div>
+        </main>
+       
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST370. 2017&copy; Valencia <br/>
+            <strong>Disclamer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+            
+           <div> <img id = "csumb" src="https://csumb.edu/sites/all/themes/custom/steinbeck/img/logos/csumb-logo-white.svg"></img></div>
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
